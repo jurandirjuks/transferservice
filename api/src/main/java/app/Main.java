e94b0d4bc83987bcbbb2a2e54c9bf7d23e6a9029
jmneto <jmneto@uolinc.com>
2019-01-15 20:51:34 -0200

    gitignore

--- api/src/main/java/app/Main.java
+++ api/src/main/java/app/Main.java
@@ -13,7 +13,6 @@ public class Main {
     public static void main(String[] args) {
         startJob();
 
-
         port(8080);
         post("/transfer","application/json",TransferResource.transfer);
     }
