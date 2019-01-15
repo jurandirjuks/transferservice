512b8d31eb3cab848c11ea6b77674aebfb68d040
jurandirjuks <jurandirjuks@gmail.com>
2019-01-15 00:48:14 -0200

    first

---
+++ api/src/main/java/resource/TransferResource.java
@@ -0,0 +1,15 @@
+package resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class TransferResource {
+
+    public static Route serveLoginPage = (Request request, Response response) -> {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TransferVO transferVO = objectMapper.readValue(request.body(), TransferVO.class);
+        return transferVO;
+    };
+}
---
+++ api/src/main/java/resource/TransferVO.java
@@ -0,0 +1,24 @@
+package resource;
+
+public class TransferVO {
+
+    private String accountName;
+
+    private Double value;
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
